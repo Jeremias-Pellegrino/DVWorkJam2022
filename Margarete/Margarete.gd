f9c313bb8c1a0179aa20493d55c04725e171f46d
Jeremias Pellegrino <jeremiaspellegrino@gmail.com>
2022-07-15 00:37:15 -0300

    minor changes

--- Margarete/Margarete.gd
+++ Margarete/Margarete.gd
@@ -2,19 +2,19 @@ extends KinematicBody2D
 
 class_name Margarete
 
-onready var _animated_sprite: AnimatedSprite = $AnimatedSprite
+onready var animation: AnimatedSprite = $AnimatedSprite
 
 func _process(_delta):
 	if Input.is_action_pressed("ui_right"):
-		_animated_sprite.play("run")
-		_animated_sprite.flip_h = false
+		animation.play("run")
+		animation.flip_h = false
 
 	elif Input.is_action_pressed("ui_left"):
-		_animated_sprite.play("run")
-		_animated_sprite.flip_h = true
+		animation.play("run")
+		animation.flip_h = true
 		
 	else:
-		_animated_sprite.play("default")
+		animation.play("default")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
