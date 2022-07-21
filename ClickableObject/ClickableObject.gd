6b113abacd126473baf2b4da7a95ec637da3bbf5
Jeremias Pellegrino <jeremiaspellegrino@gmail.com>
2022-07-21 01:44:23 -0300

    Correccion shader compartido. probando stack views para organizar esquema visual

--- ClickableObject/ClickableObject.gd
+++ ClickableObject/ClickableObject.gd
@@ -1,11 +1,26 @@
 extends Area2D
 
 onready var sprite : Sprite = $Sprite
-# Hay que ajustar el script para que la animacion del glow (shader)
-# comience al hacer hover
-func _on_Area2D_mouse_entered():
-	sprite.material.set_shader_param("width", 20)
+
+export var spritePath: String
+
+func _ready():
+
+	if spritePath == null || not spritePath.empty():
+		sprite.texture = load(spritePath)
+	else:
+		pass
 	
+func _on_Area2D_mouse_entered():
+	print("PRUEBA", spritePath)
+	print(sprite.material)
+	sprite.material.set_shader_param("width", 20)
 
 func _on_Area2D_mouse_exited():
 	sprite.material.set_shader_param("width", 0)
+
+func _input_event(viewport, event, shape_idx):
+	print(viewport, event, shape_idx, spritePath)
+	if event is InputEventMouseMotion:
+		print("FUI YO")
+		
