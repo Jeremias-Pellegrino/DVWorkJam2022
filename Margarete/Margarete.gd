c43206a6e98e3e6e02bf65c5d79729c289f00d01
Jeremias Pellegrino <jeremiaspellegrino@gmail.com>
2022-07-22 17:34:59 -0300

    Adopting mock of main screen

--- Margarete/Margarete.gd
+++ Margarete/Margarete.gd
@@ -2,24 +2,34 @@ extends KinematicBody2D
 
 class_name Margarete
 
-enum state {move, idle}
+enum state {
+	MOVE, 
+	IDLE
+
+}
 onready var animation: AnimatedSprite = $AnimatedSprite
 
-func _process(_delta):
-	# if state is state.idle:
-	# 	return
-	# if InputEventMouseButton:
-	# 	animation.play("run")
-	# 	animation.flip_h = false
+var speed = Vector2(128,128)
+var last_mouse_pos = null
 
-	# elif Input.is_action_pressed("ui_left"):
-	# 	animation.play("run")
-	# 	animation.flip_h = true
+func _input(event):
+	if event.is_action_pressed("mouse_click"):
+		last_mouse_pos = get_viewport().get_mouse_position()
+
+func _physics_process(delta):
+	if last_mouse_pos:
+		var direction_vector = (last_mouse_pos - global_position)
 		
-	# else:
-	# 	animation.play("default")
-	return
+		if direction_vector.length() < 3:
+			return
+	
+		var velocity = move_and_slide(direction_vector.normalized() * speed)
+		
+#		if velocity.x > velocity.y && velocity.x < -velocity.y:
+#			$AnimationPlayer.play("MoveTop")
+#		elif velocity.x < velocity.y && velocity.x > -velocity.y:
+#			$AnimationPlayer.play("MoveDown")
+#		else:
+#			$AnimationPlayer.play("MoveSides")
+#			$Sprite.flip_h = velocity.x < 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
