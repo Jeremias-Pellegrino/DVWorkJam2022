d1b6739fc4c0e9452581b98afd829e3d6486e963
Belen Carmona <98711618+Bel3110@users.noreply.github.com>
2022-08-07 10:26:45 -0300

    Add files via upload

--- Puzzle2/Keypad Button.gd
+++ Puzzle2/Keypad Button.gd
@@ -1,6 +1,6 @@
 extends Control
 
-const password = "1234"
+const PASSWORD = "1234"
 
 onready var label = $VBoxContainer/MarginContainer/Label
 
@@ -12,58 +12,61 @@ func key_press(digit):
 
 
 func _on_Button1_pressed():
-	pass # Replace with function body.
-
+	key_press(1)
 
 
 func _on_Button2_pressed():
-	pass # Replace with function body.
+	key_press(2)
 
 
 
 func _on_Button3_pressed():
-	pass # Replace with function body.
+	key_press(3)
 
 
 
 func _on_Button4_pressed():
-	pass # Replace with function body.
+	key_press(4)
 
 
 
 func _on_Button5_pressed():
-	pass # Replace with function body.
+	key_press(5)
 
 
 
 func _on_Button6_pressed():
-	pass # Replace with function body.
+	key_press(6)
 
 
 func _on_Button7_pressed():
-	pass # Replace with function body.
+	key_press(7)
 
 
 
 func _on_Button8_pressed():
-	pass # Replace with function body.
+	key_press(8)
 
 
 
 func _on_Button9_pressed():
-	pass # Replace with function body.
+	key_press(9)
 
 
 
 func _on_Button10_pressed():
-	pass # Replace with function body.
+	label.text = ""
 
 
 
 func _on_Button11_pressed():
-	pass # Replace with function body.
+	key_press(0)
 
 
 
 func _on_Button12_pressed():
-	pass # Replace with function body.
+	if label.text == PASSWORD:
+		print('correct pasword')
+	else:
+		print('wrong password')
+		label.text = ""
